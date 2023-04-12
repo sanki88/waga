ea92c949baea51bfd96c16db5aa57ce1fb3048a0
sanki88 <49454380+sanki88@users.noreply.github.com>
2023-04-12 12:43:16 +0200

    finish app

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const oblicz = document.querySelector("#oblicz");
 let result = document.querySelector(".result");
+let resultCNG = document.querySelector(".resultCNG");
+let resultElektryk = document.querySelector(".resultElektryk");
 
 
 oblicz.addEventListener("click", function(){
    //zmienne
    //pobieranie danych z pól
     let waga = document.querySelector("#waga").value;
-    let paliwo = document.querySelector("#paliwo").value;
     let pojemnosc = document.querySelector("#pojemnosc").value;
+    let paliwo = document.querySelector("#paliwo").value;
     //waga paliwa
     const wagaPaliwa = 0.83;
 
@@ -15,39 +17,88 @@ oblicz.addEventListener("click", function(){
     let paliwoInt = parseInt(paliwo);
     let pojemnoscInt = parseInt(pojemnosc);
     let wagazKierowca = wagaInt + 75;
-    let procentZatankowanegoZbiornika = (100*paliwoInt)/pojemnoscInt;
+    let procentZatankowanegoZbiornika = 100-paliwoInt-10;
+    let procentIleLitrow = (procentZatankowanegoZbiornika*pojemnoscInt)/100;
+    let litryNaKg = procentIleLitrow * wagaPaliwa;
     let zbiornik90procent = pojemnoscInt * 0.9;
     let tankowanieDo90 = zbiornik90procent - paliwoInt;
-    let wagaCalkowita = wagazKierowca + (tankowanieDo90 * wagaPaliwa);
+    let wagaCalkowita = wagazKierowca + litryNaKg;
 
    
 
     
     //działania
     let suma = wagaInt + pojemnoscInt;
-   // console.log(suma);
-    console.log("pojemność zatankowanego zbiornika")
+    // console.log(suma);
+    //console.log("pojemność zatankowanego zbiornika")
     console.log(procentZatankowanegoZbiornika);
-    console.log(zbiornik90procent);
-    console.log(tankowanieDo90);
+    console.log(procentIleLitrow);
+    console.log(litryNaKg);
+    // console.log(zbiornik90procent);
+    // console.log(tankowanieDo90);
 
     result.innerHTML=`
     <br>
-    <div class="card">
-    <div class="card-body">
-
-    <h3>wynik:<b> ${wagaCalkowita}</b> kg,<br><br><br></h3>
-    <p class="font-monospace">obliczenia:</p>
-    waga autobusu z kierowcą:<b> ${wagazKierowca}</b>,<br>
-    procent zatankowanego zbiornika:<b> ${procentZatankowanegoZbiornika}</b>, <br>
-    90% całego zbiornika:<b> ${zbiornik90procent}</b>, <br>
-    ilość paliwa które należy wlać żeby było 90%:<b> ${tankowanieDo90}</b>, <br>
-
-    </div>
-    </div>
-    
+    <h3>wynik:<b> ${wagaCalkowita}</b> kg</h3>
     `;
 
 
 
 })
+
+obliczCNG.addEventListener("click", function(){
+    //zmienne
+    //pobieranie danych z pól
+     let wagaCNG = document.querySelector("#wagaCNG").value;
+     let pojemnoscCNG = document.querySelector("#pojemnoscCNG").value;
+     let paliwoCNG = document.querySelector("#paliwoCNG").value;
+     let wagaCNGInt = parseInt(wagaCNG);
+     let paliwoCNGInt = parseInt(paliwoCNG);
+     let pojemnoscCNGInt = parseInt(pojemnoscCNG);
+     let wagazKierowcaCNG = wagaCNGInt + 75;
+     const wagaZatankowanegoZbiornikaCNG = 189;
+     let procentZatankowanegoZbiornikaCNG = 100-paliwoCNGInt-10;
+     let procentIleLitrowCNG = (procentZatankowanegoZbiornikaCNG*wagaZatankowanegoZbiornikaCNG)/100;
+     let wagaCalkowitaCNG = procentIleLitrowCNG + wagazKierowcaCNG;
+    
+
+    
+ 
+
+     console.log(procentZatankowanegoZbiornikaCNG);
+     console.log(procentIleLitrowCNG);
+     console.log(wagaCalkowitaCNG);
+
+
+     
+
+ 
+     resultCNG.innerHTML=`
+     <br>
+     <h3>wynik:<b>${wagaCalkowitaCNG}</b> kg</h3>
+     `;
+    });
+
+
+    obliczElektryk.addEventListener("click", function(){
+        //zmienne
+        //pobieranie danych z pól
+         let wagaElektryk = document.querySelector("#wagaElektryk").value;
+         let wagaElektrykInt = parseInt(wagaElektryk);
+         let wagaCalkowitaElektryk = wagaElektrykInt + 75;
+    
+        
+     
+    
+         console.log(wagaElektrykInt);
+
+    
+    
+         
+    
+     
+         resultElektryk.innerHTML=`
+         <br>
+         <h3>wynik:<b>${wagaCalkowitaElektryk}</b> kg</h3>
+         `;
+        })
